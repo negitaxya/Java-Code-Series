56910040aa4fded1882c9d70461631115faed437
negitaxya <taniyabca24b@hierank.org>
2025-12-31 12:12:26 +0530

    Add files via upload

---
+++ literals_03.java
@@ -0,0 +1,37 @@
+// file name is always the same as the public class name
+public class literals_03 {
+
+    public static void main(String[] args) {
+        // integer literal
+        int num1 = 100;
+        short snum = 50;
+        byte bynum = 25;
+        // long always use L or l 
+            long lnum = 1000L;
+
+        // floating-point literal
+        // float always used f & F 
+        float fnum = 10.5f;
+        double num2 = 99.99;
+
+        // character literal
+        char letter = 'A';
+
+        // string literal
+        String message = "Hello, Java!";
+
+        // boolean literal
+        boolean isJavaFun = true;
+
+        // printing the literals
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(letter);
+        System.out.println(message);
+        System.out.println(isJavaFun);
+        System.out.println(fnum);
+        System.out.println(snum);
+        System.out.println(bynum);
+        System.out.println(lnum);
+    }
+}
